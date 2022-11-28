a54a643056f5dba4a6e23289c480deb864fe4846
dolimpio <olimpiosilvadavid@gmail.com>
2022-11-28 13:33:49 +0100

    incluyendo en paquetes

--- src/main/java/es/uvigo/dagss/recetas/daos/CitaDAO.java
+++ src/main/java/es/uvigo/dagss/recetas/daos/CitaDAO.java
@@ -1,3 +1,11 @@
+package es.uvigo.dagss.recetas.daos;
+
+import es.uvigo.dagss.recetas.entidades.Cita;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 public interface CitaDAO extends JpaRepository<Cita, String> {
     List<Cita> findById(long id);
     List<Cita> findByMedico(String dni);
--- src/main/java/es/uvigo/dagss/recetas/daos/FarmaciaDAO.java
+++ src/main/java/es/uvigo/dagss/recetas/daos/FarmaciaDAO.java
@@ -1,10 +1,18 @@
+package es.uvigo.dagss.recetas.daos;
+
+import es.uvigo.dagss.recetas.entidades.Farmacia;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 public interface FarmaciaDAO extends JpaRepository<Farmacia, String> {
     List<Farmacia> findByNombreEstablecimientoContaining(String nombre);
     List<Farmacia> findByIdFarmaceutico(long id);
     List<Farmacia> findByEmailContaining(String email);
     List<Farmacia> findByTelefonoContaining(String telefono);
     List<Farmacia> findByEstadoContaining(EstadoCentroSalud estado);
-    
+
     List<Farmacia> findByDireccion(String domicilio);
     List<Farmacia> findByDireccionCodigoPostal(String codigoPostal);
     List<Farmacia> findByDireccionLocalidad(String localidad);
