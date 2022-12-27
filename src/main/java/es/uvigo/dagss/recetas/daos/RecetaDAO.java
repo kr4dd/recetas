874c5c876f9b57ae54147f31958db0ddce79d7d0
Miranda <71145273+MirandaCarou@users.noreply.github.com>
2022-12-27 18:31:56 +0100

    hechos dao receta y medico  falta comprobar

---
+++ src/main/java/es/uvigo/dagss/recetas/daos/RecetaDAO.java
@@ -0,0 +1,28 @@
+package es.uvigo.dagss.recetas.daos;
+import es.uvigo.dagss.recetas.entidades.Prescripcion;
+import es.uvigo.dagss.recetas.entidades.Receta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+public interface RecetaDAO extends JpaRepository<Receta, String> {
+    /*DAO HECHO POR MIRI*/
+
+    /*, desde la opción "Gestión de receta" se accede a un formulario, 
+    donde introduciendo el nº de tarjeta sanitaria de un paciente se accederá
+     a la lista de recetas que este estén en vigor para ese paciente, visualizándose 
+     las recetas existentes en el sistema disponibles para ser servidas al paciente 
+     indicado desde la fecha actual*/
+     /*
+      Para cada receta se mostrará los datos del medicamento de la correspondiente 
+      prescripción (principio activo, fabricante), los datos del médico que hizo la 
+      prescripción las fechas de validez de cada receta (inical y final) y el número
+       de unidades ("cajas") que se servirán en la receta
+     */
+    @Query("SELECT r FROM Recetas AS r"+
+            "JOIN FETCH " +
+            "r.prescripcion p WHERE p.paciente.numTarjetaSanitaria LIKE %:patron%")
+    List<Receta> findByNumTargetaSanit(@Param("patron") String patron);
+    
+}
